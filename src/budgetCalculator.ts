8fa5f9c2b7b6cc8ad43ca0a87b3594fedd47220a
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-13 16:48:29 +0200

    refactor(budget): add validations and clean code

--- src/budgetCalculator.ts
+++ src/budgetCalculator.ts
@@ -1,4 +1,3 @@
-//Definim el tipus unió per als missatges (Union Type)
 type BudgetStatus =
   | "Sota pressupost ✈️"
   | "Dins pressupost ✅"
@@ -7,18 +6,29 @@ type BudgetStatus =
 export const calculateBudgetStatus = (
   totalExpenses: number,
   budget: number,
-): BudgetStatus => { //Firmem la funció amb un tipus de retorn
+): BudgetStatus => {
+  if (budget <= 0) {
+    throw new Error("El pressupost ha de ser superior a 0");
+  }
+
+  if (totalExpenses < 0) {
+    throw new Error("Les despeses no poden ser negatives");
+  }
+
   const percentage = (totalExpenses / budget) * 100;
 
   if (percentage < 80) {
     return `Sota pressupost ✈️`;
   }
 
-  if (percentage >= 80 && percentage <= 100) {
+  if (percentage <= 100) {
     return `Dins pressupost ✅`;
   }
 
   return `Sobre pressupost ⚠️`;
 };
 
-console.log(calculateBudgetStatus(850, 1000));
+// console.log(calculateBudgetStatus(850, 1000));
+// console.log(calculateBudgetStatus(500, 1000));
+// console.log(calculateBudgetStatus(1200, 1000));
+// console.log(calculateBudgetStatus(100, 0));
